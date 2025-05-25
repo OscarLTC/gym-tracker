e737d5c3d82f5d2e10a62c6b4ef0b097cbc33c3f
OscarLTC <oscartazacordero@gmail.com>
2025-05-25 10:18:08 -0500

    init: setup project

---
+++ app/(tabs)/new.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+const NewTab = () => {
+  return (
+    <View>
+      <Text
+        style={{
+          color: "white",
+        }}
+      >
+        asd
+      </Text>
+    </View>
+  );
+};
+
+export default NewTab;
